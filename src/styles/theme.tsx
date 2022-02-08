f52709a2352b23bc767098c65efd0e889ec3a375
iamidlek <iamidlek@naver.com>
2022-02-09 00:48:27 +0900

    feat: use combined store state

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,17 +1,17 @@
-import { useSelector } from "react-redux";
-import { DarkModeState } from "../redux/thememode";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, themeDefault } from "../themes/default";
-import { GlobalStyles } from "./globals";
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, themeDefault } from '../themes/default';
+import { GlobalStyles } from './globals';
+import { RootState } from '../redux/modules';
 
 const Theme = ({ children }: any) => {
-  const isDark = useSelector((state: DarkModeState) => state.isDark);
-  return (
-    <ThemeProvider theme={isDark ? darkTheme : themeDefault}>
-      <GlobalStyles {...themeDefault} />
-      {children}
-    </ThemeProvider>
-  );
+	const isDark = useSelector((state: RootState) => state.thememode.isDark);
+	return (
+		<ThemeProvider theme={isDark ? darkTheme : themeDefault}>
+			<GlobalStyles {...themeDefault} />
+			{children}
+		</ThemeProvider>
+	);
 };
 
 export default Theme;
