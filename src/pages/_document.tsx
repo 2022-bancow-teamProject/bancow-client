2333f724d858299e00bb6dbcc51b971755e287df
white9p <kmjkmj9905@naver.com>
2022-02-09 18:18:32 +0900

    fix: Add Validation in section3 in farm page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,20 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ko">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="ko">
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
